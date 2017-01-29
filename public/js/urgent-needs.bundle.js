de7538e79c67739103a38ec172ba581b71379aa1
Deric Cain <deric.cain@gmail.com>
2017-01-29 17:04:53 -0600

    Dang prod assets!!

---
+++ public/js/urgent-needs.bundle.js
@@ -0,0 +1,1 @@
+webpackJsonp([2],{36:function(t,n,e){"use strict";function s(){r.submit(function(t){t.preventDefault(),i.a.post(this.action,$(this).serialize()).then(function(t){toastr.success("You're form was submitted!"),o()})["catch"](function(t){})})}function o(){document.getElementById("urgent-needs-form").reset()}function u(){s()}var c=e(1),i=e.n(c),r=$("#urgent-needs-form");u()}},[36]);
